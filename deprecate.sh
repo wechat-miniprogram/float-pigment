fb10eb1f49001b834781b722576191a640051a22
boquanfu <boquanfu@tencent.com>
2024-12-02 16:54:14 +0800

    build: update publish scripts

---
+++ deprecate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Missing version. Usage: $0 [VERSION]"
+    exit -1
+fi
+
+VERSION="$1"
+PROJECTS=$(egrep '^[ \t]*"(.+)",$' Cargo.toml | sed -E 's/^[ \t]*"(.+)",$/\1/g')
+
+# cargo yank
+echo "Ready to deprecate version ${VERSION}."
+for PROJECT in $PROJECTS; do
+    echo ""
+    echo "Deprecating ${PROJECT}..."
+    cargo yank --version "${VERSION}" "${PROJECT}"
+done
