3009716961d8435db1e07d8cf9b62d645d92f8ec
RomChung <thelns@vip.qq.com>
2025-05-21 16:42:11 +0800

    test: refactor aspect-ratio test

--- float-pigment-forest/tests/custom/css_aspect_ratio.rs
+++ float-pigment-forest/tests/custom/css_aspect_ratio.rs
@@ -5,138 +5,33 @@ use float_pigment_layout::{DefLength, OptionNum, OptionSize};
 unsafe fn as_ref<'a>(node: *mut Node) -> &'a Node {
     &*node
 }
+
 #[test]
 pub fn aspect_ratio_in_flex_row() {
-    unsafe {
-        let root = as_ref(Node::new_ptr());
-
-        let container = as_ref(Node::new_ptr());
-        container.set_width(DefLength::Points(Len::from_f32(700.)));
-        container.set_height(DefLength::Points(Len::from_f32(300.)));
-        container.set_display(Display::Flex);
-        container.set_flex_direction(FlexDirection::Row);
-        root.append_child(convert_node_ref_to_ptr(container));
-
-        let child_a = as_ref(Node::new_ptr());
-        child_a.set_width(DefLength::Points(Len::from_f32(100.)));
-        child_a.set_height(DefLength::Points(Len::from_f32(100.)));
-        child_a.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_a));
-
-        let child_b = as_ref(Node::new_ptr());
-        child_b.set_width(DefLength::Auto);
-        child_b.set_height(DefLength::Points(Len::from_f32(100.)));
-        child_b.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_b));
-
-        let child_c = as_ref(Node::new_ptr());
-        child_c.set_width(DefLength::Points(Len::from_f32(100.)));
-        child_c.set_height(DefLength::Auto);
-        child_c.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_c));
-
-        let child_d = as_ref(Node::new_ptr());
-        child_d.set_width(DefLength::Points(Len::from_f32(100.)));
-        child_d.set_height(DefLength::Auto);
-        child_d.set_align_self(AlignSelf::FlexStart);
-        child_d.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_d));
-
-        root.layout(
-            OptionSize::new(OptionNum::some(Len::from_f32(800.)), OptionNum::none()),
-            Size::new(Len::from_f32(0.), Len::from_f32(0.)),
-        );
-        println!(
-            "{}",
-            root.dump_to_html(
-                DumpOptions {
-                    recursive: true,
-                    layout: true,
-                    style: DumpStyleMode::None
-                },
-                0
-            )
-        );
-
-        assert_eq!(child_a.layout_position().width, 100.);
-        assert_eq!(child_a.layout_position().height, 100.);
-
-        assert_eq!(child_b.layout_position().width, 75.);
-        assert_eq!(child_b.layout_position().height, 100.);
-
-        assert_eq!(child_c.layout_position().width, 100.);
-        assert_eq!(child_c.layout_position().height, 300.);
-
-        assert_eq!(child_d.layout_position().width, 100.);
-        assert_eq!(child_d.layout_position().height.round(), 133.);
-    }
+    assert_xml!(
+        r#"
+        <div style="width: 700px; height: 300px; display: flex; flex-direction: row;">
+            <div style="width: 100px; height: 100px; aspect-ratio: 0.75 / 1;" expect_width="100" expect_height="100"></div>
+            <div style="width: auto; height: 100px; aspect-ratio: 0.75 / 1;" expect_width="75" expect_height="100"></div>
+            <div style="width: 100px; height: auto; aspect-ratio: 0.75 / 1;" expect_width="100" expect_height="300"></div>
+            <div style="width: 100px; height: auto; aspect-ratio: 0.75 / 1; align-self: flex-start" expect_width="100" expect_height="133"></div>
+        </div>
+    "#
+    )
 }
 
 #[test]
 pub fn aspect_ratio_in_flex_column() {
-    unsafe {
-        let root = as_ref(Node::new_ptr());
-
-        let container = as_ref(Node::new_ptr());
-        container.set_width(DefLength::Points(Len::from_f32(300.)));
-        container.set_height(DefLength::Points(Len::from_f32(600.)));
-        container.set_display(Display::Flex);
-        container.set_flex_direction(FlexDirection::Column);
-        root.append_child(convert_node_ref_to_ptr(container));
-
-        let child_a = as_ref(Node::new_ptr());
-        child_a.set_width(DefLength::Points(Len::from_f32(100.)));
-        child_a.set_height(DefLength::Points(Len::from_f32(100.)));
-        child_a.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_a));
-
-        let child_b = as_ref(Node::new_ptr());
-        child_b.set_width(DefLength::Auto);
-        child_b.set_height(DefLength::Points(Len::from_f32(100.)));
-        child_b.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_b));
-
-        let child_c = as_ref(Node::new_ptr());
-        child_c.set_width(DefLength::Auto);
-        child_c.set_height(DefLength::Points(Len::from_f32(100.)));
-        child_c.set_aspect_ratio(Some(0.75 / 1.));
-        child_c.set_align_self(AlignSelf::FlexStart);
-        container.append_child(convert_node_ref_to_ptr(child_c));
-
-        let child_d = as_ref(Node::new_ptr());
-        child_d.set_width(DefLength::Points(Len::from_f32(100.)));
-        child_d.set_height(DefLength::Auto);
-        child_d.set_aspect_ratio(Some(0.75 / 1.));
-        container.append_child(convert_node_ref_to_ptr(child_d));
-
-        root.layout(
-            OptionSize::new(OptionNum::some(Len::from_f32(375.)), OptionNum::none()),
-            Size::new(Len::from_f32(0.), Len::from_f32(0.)),
-        );
-        println!(
-            "{}",
-            root.dump_to_html(
-                DumpOptions {
-                    recursive: true,
-                    layout: true,
-                    style: DumpStyleMode::None
-                },
-                0
-            )
-        );
-
-        assert_eq!(child_a.layout_position().width, 100.);
-        assert_eq!(child_a.layout_position().height, 100.);
-
-        assert_eq!(child_b.layout_position().width, 300.);
-        assert_eq!(child_b.layout_position().height, 100.);
-
-        assert_eq!(child_c.layout_position().width, 75.);
-        assert_eq!(child_c.layout_position().height, 100.);
-
-        assert_eq!(child_d.layout_position().width, 100.);
-        assert_eq!(child_d.layout_position().height.round(), 133.);
-    }
+    assert_xml!(
+        r#"
+        <div style="width: 300px; height: 600px; display: flex; flex-direction: column;">
+            <div style="width: 100px; height: 100px; aspect-ratio: 0.75 / 1;" expect_width="100" expect_height="100"></div>
+            <div style="width: auto; height: 100px; aspect-ratio: 0.75 / 1;" expect_width="300" expect_height="100"></div>
+            <div style="width: auto; height: 100px; aspect-ratio: 0.75 / 1; align-self: flex-start" expect_width="75" expect_height="100"></div>
+            <div style="width: 100px; height: auto; aspect-ratio: 0.75 / 1;" expect_width="100" expect_height="133"></div>
+        </div>
+    "#
+    )
 }
 
 #[test]
@@ -221,43 +116,46 @@ pub fn aspect_ratio_with_flex_wrap() {
     }
 }
 
-// #[test]
-// pub fn aspect_ratio_1() {
-//     unsafe {
-//         let root = as_ref(Node::new_ptr(Some(0), None));
+#[test]
+pub fn aspect_ratio_with_flex_no_wrap_1() {
+    unsafe {
+        let root = as_ref(Node::new_ptr());
 
-//         let container = as_ref(Node::new_ptr(Some(1), None));
-//         container.set_height(DefLength::Points(50.));
-//         container.set_display(Display::Flex);
-//         container.set_flex_direction(FlexDirection::Column);
-//         container.set_flex_wrap(FlexWrap::NoWrap);
-//         container.set_align_items(AlignItems::Stretch);
-//         root.append_child(container.self_ptr().unwrap());
+        let container = as_ref(Node::new_ptr());
+        container.set_height(DefLength::Points(Len::from_f32(50.)));
+        container.set_display(Display::Flex);
+        container.set_flex_direction(FlexDirection::Column);
+        container.set_flex_wrap(FlexWrap::NoWrap);
+        container.set_align_items(AlignItems::Stretch);
+        root.append_child(convert_node_ref_to_ptr(container));
 
-//         let child_a = as_ref(Node::new_ptr(Some(2), None));
-//         child_a.set_aspect_ratio(AspectRatio::Ratio(Number::F32(1.), Number::F32(1.)));
-//         container.append_child(child_a.self_ptr().unwrap());
+        let child_a = as_ref(Node::new_ptr());
+        child_a.set_aspect_ratio(Some(1. / 1.));
+        container.append_child(convert_node_ref_to_ptr(child_a));
 
-//         root.layout(OptionNum::some(375.), OptionNum::none());
-//         println!(
-//             "{}",
-//             root.dump_to_html(
-//                 DumpOptions {
-//                     recursive: true,
-//                     layout: true,
-//                     style: DumpStyleMode::None
-//                 },
-//                 0
-//             )
-//         );
+        root.layout(
+            OptionSize::new(OptionNum::some(Len::from_f32(375.)), OptionNum::none()),
+            Size::new(Len::from_f32(0.), Len::from_f32(0.)),
+        );
+        println!(
+            "{}",
+            root.dump_to_html(
+                DumpOptions {
+                    recursive: true,
+                    layout: true,
+                    style: DumpStyleMode::None
+                },
+                0
+            )
+        );
 
-//         assert_eq!(child_a.layout_position().width, 375.);
-//         assert_eq!(child_a.layout_position().height, 50.);
-//     }
-// }
+        assert_eq!(child_a.layout_position().width, 375.);
+        assert_eq!(child_a.layout_position().height, 50.);
+    }
+}
 
 #[test]
-pub fn aspect_ratio_2() {
+pub fn aspect_ratio_with_flex_no_wrap_2() {
     unsafe {
         let root = as_ref(Node::new_ptr());
 
@@ -659,7 +557,7 @@ pub fn aspect_ratio_block_size_with_box_sizing_and_writing_mode() {
 }
 
 #[test]
-pub fn apsect_ratio_writing_mode_streched() {
+pub fn aspect_ratio_writing_mode_stretched() {
     assert_xml!(
         r#"
         <div>
